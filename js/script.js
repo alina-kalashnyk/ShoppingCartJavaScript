737a94600963e27d5ea8507ca99c83891bc21d19
Alina Kalashnyk <kalashnykalinka@gmail.com>
2019-08-08 00:09:15 +0300

    Added localStorage and product counter.

--- js/script.js
+++ js/script.js
@@ -1,19 +1,47 @@
 class ServiceProducts {
-    constructor(containerProducts, productsCatalog) {
+    constructor(containerProducts, containerCounter, productsCatalog) {
         this.container = document.querySelector(containerProducts);
+        this.containerCounter = document.querySelector(containerCounter);
         this.productsCatalog = productsCatalog;
         this.create();
     }
 
     create() {
         let wrapper = document.createElement('slot');
+
+        let products = serviceStore.getProducts();
+        this.containerCounter.innerText = products.length;
+
         for (let i = 0; i < this.productsCatalog.length; i++) {
+            let index = products.indexOf(this.productsCatalog[i].id);
+            if (index === -1) {
+                var activeClass = '';
+                var activeText = 'Add to cart';
+            } else {
+                var activeClass = ' btn-active';
+                var activeText = 'Remove from cart';
+            }
+
             let item  = this.getElement({tagName: 'div', className: 'item'});
             let name  = this.getElement({tagName: 'div', className: 'name', innerText: this.productsCatalog[i].name});
             let img   = this.getElement({tagName: 'div', className:'img', backgroundImage: `url(${this.productsCatalog[i].img})`});
             let price = this.getElement({tagName: 'div', className: 'price', innerText: this.productsCatalog[i].price.toLocaleString()});
-            let btn   = this.getElement({tagName: 'button', className: 'btn', innerText: `Add to cart`});
+            let btn   = this.getElement({tagName: 'button', className: 'btn'+activeClass, innerText: activeText, id: this.productsCatalog[i].id});
 
+            btn.addEventListener('click', function () {
+                let id = this.getAttribute('data-id');
+                let result = serviceStore.putProducts(id);
+
+                serviceProducts.containerCounter.innerText = result.products.length;
+
+                if (result.pushProduct) {
+                    this.classList.add('btn-active');
+                    this.innerText = 'Remove from cart';
+                } else {
+                    this.classList.remove('btn-active');
+                    this.innerText = 'Add to cart';
+                }
+            });
             item.appendChild(name);
             item.appendChild(img);
             item.appendChild(price);
@@ -35,6 +63,9 @@ class ServiceProducts {
         if('backgroundImage' in options) {
             element.style.backgroundImage = options.backgroundImage;
         }
+        if('id' in options) {
+            element.setAttribute('data-id', options.id );
+        }
         return element;
     }
 
@@ -42,4 +73,4 @@ class ServiceProducts {
     }
 }
 
-let serviceProducts = new ServiceProducts('.container-products', productsCatalog);
+let serviceProducts = new ServiceProducts('.container-products', '.container-counter', productsCatalog);
---
+++ js/serviceStore.js
@@ -0,0 +1,34 @@
+class ServiceStore {
+    constructor() {
+    }
+
+    getProducts() {
+        let products = [];
+        let productsLocalStorage = localStorage.getItem('products');
+        if (productsLocalStorage !== null) {
+            products = JSON.parse(productsLocalStorage);
+        }
+        return products;
+    }
+
+    putProducts(id) {
+        let products = this.getProducts();
+        let index = products.indexOf(id);
+        if (index === -1) {
+            products.push(id);
+            var pushProduct = true;
+        } else {
+            products.splice(index, 1);
+            var pushProduct = false;
+        }
+
+        localStorage.setItem('products', JSON.stringify(products));
+
+        return {
+            pushProduct : pushProduct,
+            products: products
+        }
+    }
+}
+
+let serviceStore = new ServiceStore();
